3999d8cbb68ad5f514f1ba9d073d274da7fbdc9f
Christian Charbonnier <107008498+charbo05@users.noreply.github.com>
2025-07-31 01:22:17 +0000

    maquetado inicial

---
+++ feviva/js/main.js
@@ -0,0 +1,6 @@
+// Comportamiento inicial de la app
+document.getElementById("btnEvangelio").addEventListener("click", function () {
+  // Por ahora, mostramos un alert. Más adelante cargaremos contenido real.
+  alert("Hoy es: 31 de julio\nEvangelio: Mateo 13, 36-43\n\"El que tenga oídos, que oiga\"");
+});
+
